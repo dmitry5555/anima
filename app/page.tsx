bd032762190f10a8ac618ca89dbee75f8c0881e1
Dmitrii Larionov <mikrolux@gmail.com>
2024-12-12 14:51:49 +0700

    db connection

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,5 @@
 'use client'
+import DataPage from "@/components/DB";
 import ResponsiveThree from "@/components/ExampleAnimation";
 import MetaMaskConnect from "@/components/MetaMask";
 // connect wallet
@@ -11,15 +12,24 @@ import Link from "next/link"
 export default function Home() {
   return (
     <div className="bg-slate-200 font-bold gap-8 p-12 flex flex-col h-screen">
-	  <div className="mx-auto max-w-screen-lg w-full flex flex-col gap-4 p-8 rounded-xl bg-white">
-	  <div className="gap-2 flex">
+	  <div className="mx-auto max-w-screen-lg w-full flex flex-col gap-4 p-8  bg-orange-100">
+	  <div className="gap-2 flex flex-col">
+			<h2>Wallet</h2>
 			<MetaMaskConnect />
 		</div>	
-		<div className="gap-2 flex">
-			<Link className="px-2 py-1 border-2 rounded-xl border-black" href="/">Select track</Link>
-			<Link className="px-2 py-1 border-2 rounded-xl border-black" href="/">Choose token</Link>
+		<div className="gap-2 flex flex-col">
+			<h2>Token actions</h2>
+			<div className="gap-2 flex">
+				<Link className="px-2 py-1 border-2 rounded-xl border-black" href="/">Select track</Link>
+				<Link className="px-2 py-1 border-2 rounded-xl border-black" href="/">Choose token</Link>
+			</div>
 		</div>
-		<div className="flex h-96 w-full">
+		<div className="gap-2 flex flex-col">
+			<h2>DB Data</h2>
+			<DataPage />
+		</div>
+		<div className="flex h-96 w-full flex-col gap-2">
+			<h2>Three-js</h2>
 			<ResponsiveThree />
 		</div>
 			
---
+++ components/DB.tsx
@@ -0,0 +1,51 @@
+
+import { useEffect, useState } from 'react';
+
+const DataPage = () => {
+  const [data, setData] = useState<any>(null);
+  const [error, setError] = useState<string | null>(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
+      const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
+
+      try {
+        const response = await fetch(`${supabaseUrl}/rest/v1/users?select=*`, {
+          method: 'GET',
+          headers: {
+            'Content-Type': 'application/json',
+            'apikey': supabaseKey,
+            'Authorization': `Bearer ${supabaseKey}`,
+          },
+        });
+
+        if (!response.ok) {
+          throw new Error('Failed to fetch data');
+        }
+
+        const result = await response.json();
+        setData(result);
+		console.log('result:', result);
+      } catch (error) {
+        setError(error.message);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  if (error) return <p>Error: {error}</p>;
+
+  return (
+    <div>
+      {data ? (
+        <pre className='bg-slate-100 text-sm p-2'>{JSON.stringify(data, null, 2)}</pre>
+      ) : (
+        <p>Loading...</p>
+      )}
+    </div>
+  );
+};
+
+export default DataPage;
